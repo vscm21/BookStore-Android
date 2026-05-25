4840d018d5a6dc3cdc3b49df9565aa4686674e1a
Vanessa Madeira <vanessa.madeira@pt.softinsa.com>
2026-05-25 18:28:20 +0100

    feat: add BooksViewModel with Paging 3 and favorites

---
+++ app/src/main/java/com/example/bookstoreapp/presentation/viewmodel/BooksViewModel.kt
@@ -0,0 +1,45 @@
+package com.example.bookstoreapp.presentation.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.cachedIn
+import com.example.bookstoreapp.data.local.entity.FavoriteBookEntity
+import com.example.bookstoreapp.domain.repository.BooksRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class BooksViewModel @Inject constructor(
+    private val booksRepository: BooksRepository
+) : ViewModel() {
+
+    //-- [API with Paging 3] ---
+    fun getBooks(query: String) = Pager(
+        config = PagingConfig(
+            pageSize = 20,
+            enablePlaceholders = false
+        ),
+        pagingSourceFactory = { booksRepository.getBooksPagingSource(query = query) })
+        .flow.cachedIn(viewModelScope)
+
+    //---- [DB operations]
+    fun insertFavoriteBookFromDB(book: FavoriteBookEntity) = viewModelScope.launch {
+        booksRepository.insertFavoriteBook(book)
+    }
+
+    fun deleteFavoriteBookFromDB(bookToDelete: FavoriteBookEntity) = viewModelScope.launch {
+        booksRepository.deleteFavoriteBook(bookToDelete)
+    }
+
+    //Continues stream, so it is a value that i will observe it
+    val favoriteBooks = booksRepository.getAllFavoriteBooks().stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5000),
+        initialValue = emptyList()
+    )
+}
